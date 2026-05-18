66cf5ff77abbac018cb69a4fe80d8ee4fcb86cb9
Erick <e936196@gmail.com>
2026-05-18 13:06:50 -0300

    Add GameOverScene

---
+++ scenes/GameOverScene.js
@@ -0,0 +1,47 @@
+export default class GameOverScene extends Phaser.Scene {
+  constructor() {
+    super('GameOverScene');
+  }
+
+  create(data) {
+    this.add.image(480, 270, 'mist-bg-placeholder');
+
+    this.add.text(480, 145, 'Você caiu na Névoa Vazia', {
+      fontFamily: 'Arial',
+      fontSize: '38px',
+      color: '#ff8fab',
+      fontStyle: 'bold',
+    }).setOrigin(0.5);
+
+    this.add.text(480, 205, `Moedas coletadas nesta tentativa: ${data.coins ?? 0}`, {
+      fontFamily: 'Arial',
+      fontSize: '20px',
+      color: '#ffd166',
+    }).setOrigin(0.5);
+
+    this.createButton(480, 300, 'Tentar novamente', () => {
+      this.scene.start('GameScene');
+    });
+
+    this.createButton(480, 365, 'Voltar ao menu', () => {
+      this.scene.start('MenuScene');
+    });
+  }
+
+  createButton(x, y, label, callback) {
+    const button = this.add.rectangle(x, y, 280, 48, 0x18324a, 0.95)
+      .setStrokeStyle(2, 0x7be7ff)
+      .setInteractive({ useHandCursor: true });
+
+    this.add.text(x, y, label, {
+      fontFamily: 'Arial',
+      fontSize: '19px',
+      color: '#f2fbff',
+      fontStyle: 'bold',
+    }).setOrigin(0.5);
+
+    button.on('pointerover', () => button.setFillStyle(0x24506f));
+    button.on('pointerout', () => button.setFillStyle(0x18324a));
+    button.on('pointerdown', callback);
+  }
+}
