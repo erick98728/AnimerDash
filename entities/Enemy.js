7920d6191a2798f9e6a3bb0acf2f67d9b166fd39
Erick <e936196@gmail.com>
2026-05-18 14:12:57 -0300

    Improve enemy damage feedback knockback and defeat events

--- entities/Enemy.js
+++ entities/Enemy.js
@@ -19,8 +19,11 @@ export default class Enemy extends Phaser.Physics.Arcade.Sprite {
     this.patrolDistance = options.patrolDistance ?? 120;
     this.startX = x;
     this.direction = options.direction ?? -1;
+    this.dropCoins = options.dropCoins ?? Phaser.Math.Between(1, 3);
+    this.dropXp = options.dropXp ?? Phaser.Math.Between(1, 2);
     this.isDefeated = false;
     this.isInvulnerable = false;
+    this.isKnockedBack = false;
   }
 
   static createTexture(scene) {
@@ -36,7 +39,7 @@ export default class Enemy extends Phaser.Physics.Arcade.Sprite {
   }
 
   update() {
-    if (this.isDefeated) return;
+    if (this.isDefeated || this.isKnockedBack) return;
 
     if (Math.abs(this.x - this.startX) > this.patrolDistance) {
       this.direction *= -1;
@@ -46,23 +49,57 @@ export default class Enemy extends Phaser.Physics.Arcade.Sprite {
     this.setFlipX(this.direction > 0);
   }
 
-  takeDamage(amount) {
+  takeDamage(amount, knockbackX = 0, knockbackY = -70) {
     if (this.isDefeated) return;
 
     this.health = Math.max(0, this.health - amount);
-    this.setTint(0xffffff);
-
-    this.scene.time.delayedCall(120, () => {
-      if (!this.isDefeated) this.clearTint();
-    });
+    this.flashOnHit();
+    this.applyKnockback(knockbackX, knockbackY);
 
     if (this.health <= 0) {
       this.defeat();
     }
   }
 
+  flashOnHit() {
+    this.setTint(0xffffff);
+    this.setAlpha(0.45);
+
+    this.scene.time.delayedCall(70, () => {
+      if (!this.isDefeated) {
+        this.setAlpha(1);
+        this.setTint(0xffd166);
+      }
+    });
+
+    this.scene.time.delayedCall(140, () => {
+      if (!this.isDefeated) {
+        this.setAlpha(0.65);
+        this.clearTint();
+      }
+    });
+
+    this.scene.time.delayedCall(210, () => {
+      if (!this.isDefeated) {
+        this.setAlpha(1);
+      }
+    });
+  }
+
+  applyKnockback(knockbackX, knockbackY) {
+    this.isKnockedBack = true;
+    this.setVelocity(knockbackX, knockbackY);
+
+    this.scene.time.delayedCall(180, () => {
+      if (!this.isDefeated) {
+        this.isKnockedBack = false;
+      }
+    });
+  }
+
   defeat() {
     this.isDefeated = true;
+    this.emit('enemy-defeated', this);
     this.disableBody(true, true);
   }
 }
