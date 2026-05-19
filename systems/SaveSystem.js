fc489f1e69ec84822e143d765788269c6b4976e6
Erick <e936196@gmail.com>
2026-05-19 09:34:12 -0300

    Expand save data for retention systems

--- systems/SaveSystem.js
+++ systems/SaveSystem.js
@@ -3,7 +3,7 @@ export default class SaveSystem {
 
   static getDefaultSave() {
     return {
-      version: 2,
+      version: 3,
       playerLevel: 1,
       xp: 0,
       coins: 0,
@@ -20,6 +20,33 @@ export default class SaveSystem {
         dash: 0,
         shuriken: 0,
       },
+      retention: {
+        lastLoginDate: null,
+        loginStreak: 0,
+        bestLoginStreak: 0,
+        claimedLoginRewardDate: null,
+        claimedDailyChestDate: null,
+        claimedThreeLevelsBonusDate: null,
+        dailyStatsDate: null,
+        dailyStats: {
+          enemiesDefeated: 0,
+          coinsCollected: 0,
+          levelsCompleted: 0,
+          noDeathCompletions: 0,
+          specialUses: 0,
+          bossesDefeated: 0,
+        },
+        lifetimeStats: {
+          lifetimeEnemiesDefeated: 0,
+          lifetimeCoinsCollected: 0,
+          lifetimeLevelsCompleted: 0,
+          lifetimeNoDeathCompletions: 0,
+          lifetimeSpecialUses: 0,
+          lifetimeBossesDefeated: 0,
+        },
+        claimedDailyMissions: {},
+        claimedAchievements: {},
+      },
       lastPlayedAt: null,
     };
   }
@@ -27,10 +54,12 @@ export default class SaveSystem {
   static normalizeSave(data) {
     const defaultSave = SaveSystem.getDefaultSave();
     const loaded = data ?? {};
+    const loadedRetention = loaded.retention ?? {};
 
     return {
       ...defaultSave,
       ...loaded,
+      version: defaultSave.version,
       upgrades: {
         ...defaultSave.upgrades,
         ...(loaded.upgrades ?? {}),
@@ -38,6 +67,20 @@ export default class SaveSystem {
       completedLevels: loaded.completedLevels ?? defaultSave.completedLevels,
       unlockedSkills: loaded.unlockedSkills ?? defaultSave.unlockedSkills,
       specialItems: loaded.specialItems ?? defaultSave.specialItems,
+      retention: {
+        ...defaultSave.retention,
+        ...loadedRetention,
+        dailyStats: {
+          ...defaultSave.retention.dailyStats,
+          ...(loadedRetention.dailyStats ?? {}),
+        },
+        lifetimeStats: {
+          ...defaultSave.retention.lifetimeStats,
+          ...(loadedRetention.lifetimeStats ?? {}),
+        },
+        claimedDailyMissions: loadedRetention.claimedDailyMissions ?? defaultSave.retention.claimedDailyMissions,
+        claimedAchievements: loadedRetention.claimedAchievements ?? defaultSave.retention.claimedAchievements,
+      },
     };
   }
 
