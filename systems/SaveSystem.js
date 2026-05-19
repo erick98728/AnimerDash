37c5aa3b24f28be44099265fe77435034ce080bc
Erick <e936196@gmail.com>
2026-05-19 09:27:52 -0300

    Expand save data for progression resources and upgrades

--- systems/SaveSystem.js
+++ systems/SaveSystem.js
@@ -3,14 +3,41 @@ export default class SaveSystem {
 
   static getDefaultSave() {
     return {
+      version: 2,
+      playerLevel: 1,
+      xp: 0,
       coins: 0,
+      rareScrolls: 0,
+      skillPoints: 0,
+      totalSkillPointsEarned: 0,
       completedLevels: [],
+      unlockedSkills: [],
+      specialItems: [],
       upgrades: {
         maxHealth: 0,
         attackDamage: 0,
         energy: 0,
+        dash: 0,
+        shuriken: 0,
       },
       lastPlayedAt: null,
+    };
+  }
+
+  static normalizeSave(data) {
+    const defaultSave = SaveSystem.getDefaultSave();
+    const loaded = data ?? {};
+
+    return {
+      ...defaultSave,
+      ...loaded,
+      upgrades: {
+        ...defaultSave.upgrades,
+        ...(loaded.upgrades ?? {}),
+      },
+      completedLevels: loaded.completedLevels ?? defaultSave.completedLevels,
+      unlockedSkills: loaded.unlockedSkills ?? defaultSave.unlockedSkills,
+      specialItems: loaded.specialItems ?? defaultSave.specialItems,
     };
   }
 
@@ -21,10 +48,7 @@ export default class SaveSystem {
         return SaveSystem.getDefaultSave();
       }
 
-      return {
-        ...SaveSystem.getDefaultSave(),
-        ...JSON.parse(rawSave),
-      };
+      return SaveSystem.normalizeSave(JSON.parse(rawSave));
     } catch (error) {
       console.warn('Não foi possível carregar o progresso. Um novo save será usado.', error);
       return SaveSystem.getDefaultSave();
@@ -32,11 +56,10 @@ export default class SaveSystem {
   }
 
   static save(data) {
-    const nextSave = {
-      ...SaveSystem.getDefaultSave(),
+    const nextSave = SaveSystem.normalizeSave({
       ...data,
       lastPlayedAt: new Date().toISOString(),
-    };
+    });
 
     localStorage.setItem(SaveSystem.key, JSON.stringify(nextSave));
     return nextSave;
