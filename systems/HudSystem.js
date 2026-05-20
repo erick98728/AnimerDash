227997087153df35591329325d8ffce74b126447
Erick <e936196@gmail.com>
2026-05-20 10:36:33 -0300

    Use real HUD icons when available with text fallback

--- systems/HudSystem.js
+++ systems/HudSystem.js
@@ -1,4 +1,5 @@
 import SaveSystem from './SaveSystem.js';
+import { ASSET_MANIFEST, getTextureKey } from '../data/assetsManifest.js';
 
 export default class HudSystem {
   constructor(scene, levelSystem) {
@@ -10,19 +11,23 @@ export default class HudSystem {
   }
 
   create() {
-    this.healthText = this.scene.add.text(24, 78, '', {
+    this.healthIcon = this.createIcon(14, 86, ASSET_MANIFEST.ui.healthIcon, '❤️');
+    this.energyIcon = this.createIcon(14, 112, ASSET_MANIFEST.ui.energyIcon, '◆');
+    this.coinIcon = this.createIcon(14, 138, ASSET_MANIFEST.collectibles.coin, '●');
+
+    this.healthText = this.scene.add.text(34, 78, '', {
       fontFamily: 'Arial',
       fontSize: '18px',
       color: '#f2fbff',
     }).setScrollFactor(0);
 
-    this.energyText = this.scene.add.text(24, 104, '', {
+    this.energyText = this.scene.add.text(34, 104, '', {
       fontFamily: 'Arial',
       fontSize: '18px',
       color: '#7be7ff',
     }).setScrollFactor(0);
 
-    this.coinText = this.scene.add.text(24, 130, '', {
+    this.coinText = this.scene.add.text(34, 130, '', {
       fontFamily: 'Arial',
       fontSize: '18px',
       color: '#ffd166',
@@ -32,6 +37,23 @@ export default class HudSystem {
       fontFamily: 'Arial',
       fontSize: '15px',
       color: '#9bb6c8',
+    }).setScrollFactor(0);
+  }
+
+  createIcon(x, y, assetEntry, fallbackSymbol) {
+    const textureKey = getTextureKey(this.scene, assetEntry, null);
+
+    if (textureKey && this.scene.textures.exists(textureKey)) {
+      const icon = this.scene.add.image(x, y, textureKey)
+        .setScrollFactor(0)
+        .setDisplaySize(18, 18);
+      return icon;
+    }
+
+    return this.scene.add.text(x, y - 8, fallbackSymbol, {
+      fontFamily: 'Arial',
+      fontSize: '15px',
+      color: '#f2fbff',
     }).setScrollFactor(0);
   }
 
